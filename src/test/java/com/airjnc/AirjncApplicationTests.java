9977be4eee71e1832af2825ecd9b6f04eb7f97b9
hoon25 <cchoon95@hanmail.net>
2022-06-22 00:24:55 +0900

    chore(#22) test폴더구조변경 및 비활성화

--- src/test/java/com/airjnc/AirjncApplicationTests.java
+++ src/test/java/com/airjnc/AirjncApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.airjnc;
+package com.airjnc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AirjncApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+//  @Test
+//  void contextLoads() {
+//  }
 
 }
