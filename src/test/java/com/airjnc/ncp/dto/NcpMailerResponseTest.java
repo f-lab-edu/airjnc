ae5f3a6f24c0dc7d1481e41aa1bdc6afb7335cd6
memoer <hanjn2842@naver.com>
2022-07-29 02:37:25 +0900

    feature(#46): 이메일 전송 API 구축 [NCP 사용]
    - NCP 에서 제공해주는 OutboundMailer Service를 사용하기로 했음
    - NCP 에 대한 API를 추후에도 사용할 가능성이 있어보여, 패키지를 새로 생성하였음
    ˆ

---
+++ src/test/java/com/airjnc/ncp/dto/NcpMailerResponseTest.java
@@ -0,0 +1,21 @@
+package com.airjnc.ncp.dto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import com.testutil.annotation.UnitTest;
+import org.junit.jupiter.api.Test;
+
+// ncp 에서 api 요청시에 보낼 수 있는 데이터의 스펙이 정해져 있고, 이에 맞춰서 잘 보내고 있는 지 확인하기 위해 테스트 진행A
+@UnitTest
+class NcpMailerResponseTest {
+
+  @Test
+  void checkNcpMailerResponseFields() throws NoSuchFieldException {
+    //when
+    Class<NcpMailerResponse> clazz = NcpMailerResponse.class;
+    String requestId = clazz.getDeclaredField("requestId").getType().getName();
+    String count = clazz.getDeclaredField("count").getType().getName();
+    //then
+    assertThat(requestId.contains("String")).isTrue();
+    assertThat(count).isEqualTo("int");
+  }
+}
