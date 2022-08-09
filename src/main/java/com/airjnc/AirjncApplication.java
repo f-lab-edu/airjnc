19921511c3e286c5264d2c69e7462906f0342836
memoer <hanjn2842@naver.com>
2022-08-10 02:05:52 +0900

    refactor: 로그인 인증 체크 AOP -> Interceptor 변경

--- src/main/java/com/airjnc/AirjncApplication.java
+++ src/main/java/com/airjnc/AirjncApplication.java
@@ -2,10 +2,8 @@ package com.airjnc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
 public class AirjncApplication {
 
   public static void main(String[] args) {
