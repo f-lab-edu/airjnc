33f146865b4bb44b3075d84c10e36d0043f75912
memoer <hanjn2842@naver.com>
2022-08-18 10:39:36 +0900

    chore:modify db schema

--- docker/schema.sql
+++ docker/schema.sql
@@ -1,15 +1,19 @@
 CREATE TABLE IF NOT EXISTS `user`
 (
-    `id`           BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `email`        VARCHAR(50)     NOT NULL,
-    `password`     VARCHAR(100)    NOT NULL,
-    `name`         VARCHAR(50)     NOT NULL,
-    `gender`       VARCHAR(10),
-    `address`      VARCHAR(200),
-    `birthdate`    TIMESTAMP,
-    `created_at`   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`   TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `deleted_at`   TIMESTAMP,
+    `id`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `email`      VARCHAR(50)     NOT NULL,
+    `password`   VARCHAR(100)    NOT NULL,
+    `name`       VARCHAR(50)     NOT NULL,
+    `gender`     VARCHAR(10),
+    `address`    VARCHAR(200),
+    `birthdate`  TIMESTAMP,
+    -- DEFAULT -> 일반 유저
+    -- HOST -> 호스팅하는 유저
+    -- ADMIN -> 관리자
+    `role`       VARCHAR(10)     NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at` TIMESTAMP,
 
     PRIMARY KEY (`id`),
     UNIQUE KEY `email-unique` (`email`)
@@ -32,6 +36,10 @@ CREATE TABLE IF NOT EXISTS `room_category`
 (
     `id`               BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `name`             VARCHAR(20)     NOT NULL,
+    -- 해당 카테고리에 속하는 방의 개수[room_count]가 2개 이상인가?
+    -- 넣은 이유 -> 클라이언트 측에서 숙소 유형 "산장, B&B" 둘 중에
+    --      산장을 택하면 "숙소에 방이 총 몇 개인가요?" 라는 문구가 안나오고
+    --      B&B을 택하면 "숙소에 방이 총 몇 개인가요?" 라는 문구가 나오게 하기 위함
     `is_several_rooms` BOOLEAN         NOT NULL,
 
     PRIMARY KEY (`id`)
@@ -42,20 +50,20 @@ CREATE TABLE IF NOT EXISTS `room`
     `id`                   BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `title`                VARCHAR(50)     NOT NULL,
     `description`          TEXT            NOT NULL,
+    `price`                INT UNSIGNED    NOT NULL,
     `bed_count`            INT UNSIGNED    NOT NULL,           -- 침대 수
     `bedroom_count`        INT UNSIGNED    NOT NULL,           -- 침실 수
     `bathroom_count`       INT UNSIGNED    NOT NULL,           -- 욕실 수
-    `limit_guests`         INT UNSIGNED    NOT NULL,           -- 최대 게스트 수
+    `room_count`           INT UNSIGNED    NOT NULL DEFAULT 1, -- 예약할 수 있는 방의 개수 (1이면 한 날짜에 1방만 예약 가능, 2이면 한 날짜에 2방 예약 가능)
+    `max_guest_count`      INT UNSIGNED    NOT NULL,           -- 최대 게스트 수
     `min_number_of_nights` INT UNSIGNED    NOT NULL,           -- 최소 숙박 일수
     `max_number_of_nights` INT UNSIGNED    NOT NULL,           -- 최대 숙박 일수
-    `room_count`           INT UNSIGNED    NOT NULL DEFAULT 1, -- 방의 개수
-    `check_in`             VARCHAR(2)      NOT NULL,           -- 체크인 시간
+    `check_in`             TIME            NOT NULL,           -- 체크인 시간
     `check_out`            VARCHAR(2)      NOT NULL,           -- 체크아웃 시간
     `address`              VARCHAR(200),
-    -- content, description 종류 3가지
-    -- IN_OPERATION : 영업 중, 게스트가 숙소를 검색하고 예약 요청을 보내거나 예약 가능 날짜를 예약할 수 있습니다.
-    -- SHUT_DOWN : 운영 중지, 게스트가 숙소를 예약하거나 검색 결과에서 찾을 수 없습니다.
-    -- DISABLED : 비활성화, 에어비앤비에서 영구적으로 숙소 비활성화
+    -- IN_OPERATION : 영업 중, 게스트가 숙소를 검색하고 예약 요청을 보내거나 예약 가능 날짜를 예약할 수 있습니다. [영업 중]
+    -- SHUT_DOWN : 운영 중지, 게스트가 숙소를 예약하거나 검색 결과에서 찾을 수 없습니다. [(A)호스트가 자발적으로 했다거나, (B)호스트 처음 신청 후 어드민이 아직 수락을 안한 상태]
+    -- DISABLED : 비활성화, 에어비앤비에서 영구적으로 숙소 비활성화 [호스트가 해당 방을 제거함 -> deleted_at에 해당 날짜가 입력됨]
     `status`               VARCHAR(20)     NOT NULL,
     `created_at`           TIMESTAMP                DEFAULT CURRENT_TIMESTAMP,
     `updated_at`           TIMESTAMP                DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -72,12 +80,13 @@ CREATE TABLE IF NOT EXISTS `room`
 CREATE TABLE IF NOT EXISTS `room_reservation`
 (
     `id`          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `user_count`  INT UNSIGNED    NOT NULL, -- 예약 게스트 수
-    `total_price` INT UNSIGNED    NOT NULL, -- 예약 가격
+    `user_count`  INT             NOT NULL, -- 예약한 명 수
+    --
     `start_date`  TIMESTAMP       NOT NULL,
     `end_date`    TIMESTAMP       NOT NULL,
+    `total_price` INT UNSIGNED    NOT NULL,
+    --
     `created_at`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `deleted_at`  TIMESTAMP,                -- Soft Delete
 
     `user_id`     BIGINT UNSIGNED NOT NULL,
@@ -87,6 +96,21 @@ CREATE TABLE IF NOT EXISTS `room_reservation`
     KEY `user_id-idx` (`user_id`),
     KEY `room_id-idx` (`room_id`)
 ) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `room_reservation_date`
+(
+    `id`                  BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `date`                TIMESTAMP       NOT NULL,
+    `is_canceled`         BOOLEAN         NOT NULL DEFAULT FALSE, -- 환불 여부: TRUE->환불, FALSE->예약완료
+
+    `room_id`             BIGINT UNSIGNED NOT NULL,
+    `room_reservation_id` BIGINT UNSIGNED NOT NULL,
+
+    PRIMARY KEY (`id`),
+    KEY `room_id-idx` (`room_id`),
+    KEY `room_reservation_id-idx` (`room_reservation_id`)
+) ENGINE = InnoDB;
+
 
 CREATE TABLE IF NOT EXISTS `wish_room`
 (
@@ -112,12 +136,12 @@ CREATE TABLE IF NOT EXISTS `room_review`
     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `deleted_at` TIMESTAMP,                -- Soft Delete
 
-    `room_id`    BIGINT UNSIGNED NOT NULL,
     `user_id`    BIGINT UNSIGNED NOT NULL,
+    `room_id`    BIGINT UNSIGNED NOT NULL,
 
     PRIMARY KEY (`id`),
-    KEY `room_id-idx` (`room_id`),
-    KEY `user_id-idx` (`user_id`)
+    KEY `user_id-idx` (`user_id`),
+    KEY `room_id-idx` (`room_id`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `facility`
@@ -130,7 +154,6 @@ CREATE TABLE IF NOT EXISTS `facility`
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
--- 다대다 테이블임을 명시적으로 보여주기 위해 "__"를 중간에 넣음
 CREATE TABLE IF NOT EXISTS `room__facility`
 (
     `id`          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -151,20 +174,6 @@ CREATE TABLE IF NOT EXISTS `room_photo`
     `order`   INT UNSIGNED, -- 방에서 보여줄 사진의 우선순위 설정
 
     `room_id` BIGINT UNSIGNED NOT NULL,
-
-    PRIMARY KEY (`id`),
-    KEY `room_id-idx` (`room_id`)
-) ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS `room_price`
-(
-    `id`                    BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `month`                 INT UNSIGNED    NOT NULL, -- 1,2,3,4,5,6,7,8,9,10,11,12 월
-    `price`                 INT UNSIGNED    NOT NULL, -- 월별 기본 가격
-    `weekly_discount_price` INT UNSIGNED    NOT NULL, -- 월별 주간 할인 가격
-    `currency`              VARCHAR(20)     NOT NULL,
-
-    `room_id`               BIGINT UNSIGNED NOT NULL,
 
     PRIMARY KEY (`id`),
     KEY `room_id-idx` (`room_id`)
