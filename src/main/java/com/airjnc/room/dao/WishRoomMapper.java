f8a906456ecce55cddd3e6a84b71cdc8cd817b1a
memoer <hanjn2842@naver.com>
2022-08-19 18:06:24 +0900

    feat(#90): 위시 룸 제거 쿼리 작성

--- src/main/java/com/airjnc/room/dao/WishRoomMapper.java
+++ src/main/java/com/airjnc/room/dao/WishRoomMapper.java
@@ -1,6 +1,8 @@
 package com.airjnc.room.dao;
 
 
+import com.airjnc.room.dto.WishRoomDto;
+import java.util.Optional;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -10,4 +12,8 @@ public interface WishRoomMapper {
   int create(@Param("userId") Long userId, @Param("roomId") Long roomId);
 
   boolean existsByUserIdAndRoomId(@Param("userId") Long userId, @Param("roomId") Long roomId);
+
+  int delete(Long id);
+
+  Optional<WishRoomDto> findById(Long id);
 }
---
+++ src/main/java/com/airjnc/room/dto/WishRoomDto.java
@@ -0,0 +1,14 @@
+package com.airjnc.room.dto;
+
+import lombok.Getter;
+
+@Getter
+public class WishRoomDto {
+
+  private Long id;
+
+  private Long userId;
+
+  private Long roomId;
+
+}
