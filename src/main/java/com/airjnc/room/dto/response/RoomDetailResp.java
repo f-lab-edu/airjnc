f5e1cd497655344bc2d061a6cbf2232079bc9f7d
memoer <hanjn2842@naver.com>
2022-08-19 15:42:35 +0900

    feat(#88): 방 상세 조회를 위한 response dto 생성

---
+++ src/main/java/com/airjnc/room/dto/response/RoomDetailResp.java
@@ -0,0 +1,65 @@
+package com.airjnc.room.dto.response;
+
+import com.airjnc.room.domain.RoomStatus;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class RoomDetailResp {
+
+  private Long id;
+
+  private String title;
+
+  private String description;
+
+  private Integer bedCount;
+
+  private Integer bedroomCount;
+
+  private Integer bathroomCount;
+
+  private Integer maxGuestCount;
+
+  private Integer roomCount;
+
+  private Integer minNumberOfNights;
+
+  private Integer maxNumberOfNights;
+
+  private String checkIn;
+
+  private String checkOut;
+
+  private String address;
+
+  private RoomStatus status;
+
+  private List<String> photoList;
+
+  @Builder
+  public RoomDetailResp(Long id, String title, String description, Integer bedCount, Integer bedroomCount,
+      Integer bathroomCount, Integer maxGuestCount, Integer roomCount, Integer minNumberOfNights,
+      Integer maxNumberOfNights, String checkIn, String checkOut, String address, RoomStatus status,
+      List<String> photoList) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.bedCount = bedCount;
+    this.bedroomCount = bedroomCount;
+    this.bathroomCount = bathroomCount;
+    this.maxGuestCount = maxGuestCount;
+    this.roomCount = roomCount;
+    this.minNumberOfNights = minNumberOfNights;
+    this.maxNumberOfNights = maxNumberOfNights;
+    this.checkIn = checkIn;
+    this.checkOut = checkOut;
+    this.address = address;
+    this.status = status;
+    this.photoList = photoList;
+  }
+}
