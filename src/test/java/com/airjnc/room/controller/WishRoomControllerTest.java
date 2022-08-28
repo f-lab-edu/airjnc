da142095375d571e9917d5d84a5b8098ec55ca3a
memoer <hanjn2842@naver.com>
2022-08-28 16:07:30 +0900

    feat(#91): "/wishRooms" Controller Test Code 작성

--- src/test/java/com/airjnc/room/controller/WishRoomControllerTest.java
+++ src/test/java/com/airjnc/room/controller/WishRoomControllerTest.java
@@ -1,20 +1,27 @@
 package com.airjnc.room.controller;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.airjnc.common.service.StateService;
+import com.airjnc.common.util.enumerate.SessionKey;
 import com.airjnc.room.service.WishRoomService;
 import com.testutil.annotation.IntegrationTest;
+import com.testutil.testdata.TestUser;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @WebMvcTest(WishRoomController.class)
 @IntegrationTest
@@ -40,5 +47,19 @@ class WishRoomControllerTest {
         .andExpect(status().isCreated());
     //then
     then(wishRoomService).should().create(anyLong(), eq(roomId));
+  }
+
+  @Test
+  void getMyWishRooms() throws Exception {
+    //given
+    given(stateService.get(SessionKey.USER)).willReturn(TestUser.ID);
+    //when
+    ResultActions resultActions = mockMvc.perform(
+        get("/wishRooms")
+            .param("page", "1")
+            .param("size", "20")
+    ).andDo(print());
+    //then
+    then(wishRoomService).should().getMyWishRooms(anyLong(), any(Pageable.class));
   }
 }
