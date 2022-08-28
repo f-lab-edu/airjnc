c6410ca3fb8ed4a977d8851f1cb5ec1cb397f919
memoer <hanjn2842@naver.com>
2022-08-28 16:02:04 +0900

    feat(#91): WishRoomService 메소드 추가

--- src/main/java/com/airjnc/room/service/WishRoomService.java
+++ src/main/java/com/airjnc/room/service/WishRoomService.java
@@ -1,9 +1,12 @@
 package com.airjnc.room.service;
 
+import com.airjnc.room.dao.RoomRepository;
 import com.airjnc.room.dao.WishRoomRepository;
 import com.airjnc.room.dto.response.SimpleRoom;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +18,16 @@ public class WishRoomService {
 
   private final WishRoomValidateService wishRoomValidateService;
 
+  private final RoomRepository roomRepository;
+
   public void create(Long userId, Long roomId) {
     wishRoomValidateService.shouldBeNotWishRoom(userId, roomId);
     wishRoomRepository.create(userId, roomId);
   }
 
   public Page<SimpleRoom> getMyWishRooms(Long userId, Pageable pageable) {
-    return null;
+    Long skip = pageable.getOffset() - pageable.getPageSize();
+    List<SimpleRoom> list = roomRepository.findAllByUserId(userId, skip, pageable.getOffset());
+    return new PageImpl<>(list, pageable, wishRoomRepository.count(userId));
   }
 }
