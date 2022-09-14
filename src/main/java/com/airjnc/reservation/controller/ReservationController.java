2beba9ff3bc6e60250465df325b94620026941b4
memoer <hanjn2842@naver.com>
2022-09-14 14:46:01 +0900

    feat(#101): Controller에서 반환해주는 상태 코드 값 추가

--- src/main/java/com/airjnc/reservation/controller/ReservationController.java
+++ src/main/java/com/airjnc/reservation/controller/ReservationController.java
@@ -5,10 +5,12 @@ import com.airjnc.common.annotation.CurrentUserId;
 import com.airjnc.reservation.dto.request.ReservationReq;
 import com.airjnc.reservation.service.ReservationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/reservations")
@@ -19,6 +21,7 @@ public class ReservationController {
 
   @PostMapping("/{roomId}")
   @CheckAuth
+  @ResponseStatus(HttpStatus.CREATED)
   public void reservation(@CurrentUserId Long userId, @PathVariable Long roomId,
       @Validated @RequestBody ReservationReq req) {
     reservationService.reservation(userId, roomId, req);
